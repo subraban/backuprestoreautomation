7f8de4ac396c2461740ac12874552c3d4efad95b
subraban <136383571+subraban@users.noreply.github.com>
2023-07-10 23:54:30 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.70.0"
-    }
-   
-  }
-}
 
 provider "google" {
   credentials=file("creds.json")
