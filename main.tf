07603603d38c2f36ba5df695db33f4553573d3f6
subraban <136383571+subraban@users.noreply.github.com>
2023-07-11 00:37:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "google_storage_bucket_object" "ora_object12" {
 
 #Upload sql backup files to google storage bucket
 
-resource "google_storage_bucket" "sqlbackuptos3_bucket" {
+resource "google_storage_bucket" "sqlautobackuptostrg_bucket" {
   name     = var.sqlname
   location = var.location
   storage_class = var.storage_class
@@ -78,18 +78,18 @@ resource "google_storage_bucket" "sqlbackuptos3_bucket" {
 
 resource "google_storage_bucket_object" "sql_object1" {
   name   = var.sqlfile1
-  bucket = google_storage_bucket.sqlbackuptos3_bucket.name
+  bucket = google_storage_bucket.sqlautobackuptostrg_bucket.name
   source = var.sql1
 }
 
 resource "google_storage_bucket_object" "sql_object2" {
   name   = var.sqlfile2
-  bucket = google_storage_bucket.sqlbackuptos3_bucket.name
+  bucket = google_storage_bucket.sqlautobackuptostrg_bucket.name
   source = var.sql2
 }
 resource "google_storage_bucket_object" "sql_object3" {
   name   = var.sqlfile3
-  bucket = google_storage_bucket.sqlbackuptos3_bucket.name
+  bucket = google_storage_bucket.sqlautobackuptostrg_bucket.name
   source = var.sql3
 }
 
