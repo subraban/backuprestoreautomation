2bd268198020c6f1dfdfef1a88c5a8d4067caa15
subraban <136383571+subraban@users.noreply.github.com>
2023-07-10 23:47:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -127,3 +127,29 @@ variable "s12" {
 type=string
 default="D:/SOFTWARE/ORADATA/ORCL/ONLINELOG/O1_MF_3_LB81PX9B_.LOG"
 }
+
+variable "sqlfile1" {
+type=string
+default="DB1.bak"
+}
+variable "sqlfile2" {
+type=string
+default="DB2.bak"
+}
+variable "sqlfile3" {
+type=string
+default="DB3.bak"
+}
+
+variable "sql1" {
+type=string
+default="D:/MSSQL14.SQL2017/DB1.bak"
+}
+variable "sql2" {
+type=string
+default="D:/MSSQL14.SQL2017/DB2.bak"
+}
+variable "sql3" {
+type=string
+default="D:/MSSQL14.SQL2017/DB3.bak"
+}
